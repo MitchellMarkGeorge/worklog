fd3ccff628b0ade304b56da5fffc04baf17b9a2d
Mitchell Mark-George <mitchellmmarkgeorge@gmail.com>
2025-07-31 22:59:48 -0400

    feat: implemented main process application and window models

---
+++ src/main/models/WorklogApplication.ts
@@ -0,0 +1,73 @@
+import { WindowService } from "../services/window";
+import { app } from "electron";
+import utils from "../utils/utils";
+
+export class WorklogApplication {
+  private static instance: WorklogApplication;
+  private hasStarted: boolean;
+  private windowService: WindowService;
+
+  static getInstance(): WorklogApplication {
+    if (!this.instance) {
+      throw new Error("WorklogApplication not initialized");
+    }
+    return this.instance;
+  }
+
+  private constructor() {
+    // Initialize the application
+    this.windowService = new WindowService();
+    this.handleAppListeners();
+  }
+
+  static init() {
+    if (this.instance) {
+      throw new Error("WorklogApplication already initialized");
+    } else {
+      const gotLock = app.requestSingleInstanceLock();
+      if (!gotLock) {
+        app.quit();
+      } else {
+        app.on("second-instance", () => {
+          if (this.instance) {
+            const lastFocusedWindow =
+              this.instance.windowService.getLastFocusedWindow();
+            if (lastFocusedWindow) {
+              if (lastFocusedWindow.browserWindow.isMinimized()) {
+                lastFocusedWindow.browserWindow.restore();
+              } else {
+                lastFocusedWindow.browserWindow.focus();
+              }
+            }
+          }
+        });
+      }
+      
+      this.instance = new WorklogApplication();
+      return this.instance;
+    }
+  }
+  
+  public start() {
+    if (!this.hasStarted) {
+      this.windowService.openNewWindow()
+      this.hasStarted = true;
+    } else {
+      throw new Error("WorklogApplication already launched");
+    }
+  }
+  
+  private handleAppListeners() {
+    app.on("activate", () => {
+      if (!this.windowService.hasWindows()) {
+        this.windowService.openNewWindow();
+      }
+    });
+    
+    app.on("window-all-closed", () => {
+      if (!utils.isMac) {
+        app.quit();
+      }
+    });
+  }
+}
---
+++ src/main/utils/utils.ts
@@ -0,0 +1,19 @@
+import { nativeTheme } from 'electron';
+
+const isMac = process.platform === "darwin";
+const isWindows = process.platform === "win32";
+const isLinux = process.platform === "linux";
+const isDarkMode = nativeTheme.shouldUseDarkColors;
+const lightBackgroundColor = "#FAFAFA";
+const darkBackgroundColor = "#171717";
+const getWindowBackgroundColor = () => isDarkMode ? darkBackgroundColor : lightBackgroundColor;
+
+export default {
+    isMac,
+    isWindows,
+    isLinux,
+    isDarkMode,
+    lightBackgroundColor,
+    darkBackgroundColor,
+    getWindowBackgroundColor
+}
